6a07e4130d89c1c4ca582429228e0c7b2b7cd2f6
Victor Marra <zaffalonvictor@gmail.com>
2021-12-20 10:35:20 -0300

    Added css for form, options and input

--- src/App.js
+++ src/App.js
@@ -8,33 +8,32 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <div id="background">
-          <div>
-            <div id="auto-complete">
-              <div id="search-box">
+      <div className="App">
+        <div id="background" className="background">
+          <div className="centered">
+            <div id="auto-complete" className="auto-complete">
+              <div id="search-box" className="search-box">
                 <input
                   type="text"
                   id="search-input"
                   placeholder="Type to search..."
+                  className="search-input"
                 ></input>
-                <button>Submit</button>
+                <button
+                  onClick={() => this.submitSearch()}
+                  className="btn submit-button"
+                >
+                  Submit
+                </button>
               </div>
-              <div id="options">
-                <div id="options-list">
-                  <li id={1}>
-                    <p>item</p>
+              <div id="options" className="options">
+                <div id="options-list" className="options-list">
+                  <li id={1} className="options-item">
+                    <p className="option-text">item</p>
                   </li>
-                  <li id={2}>
-                    <p>item 2</p>
-                  </li>
-                  <li id={3}>
-                    <p>item 3</p>
-                  </li>
-                  ))
                 </div>
+                <h2>Submitted value is value</h2>
               </div>
-              <h2>Submitted value is value</h2>
             </div>
           </div>
         </div>
